c9882a750165e354eac2ef357bf07b238d0f4ce3
unknown <victor.90braz@gmail.com>
2022-12-14 09:38:28 +0100

    Added useEffect to render all events

--- src/components/ListPlayers/ListPlayers.js
+++ src/components/ListPlayers/ListPlayers.js
@@ -1,9 +1,16 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadPlayersThunk } from "../../redux/thunks/playersThunks";
 import PlayerCard from "../PlayerCard/PlayerCard";
 import ListPlayersStyles from "./ListPlayersStyles";
 
 const ListPlayers = () => {
   const { allPlayers } = useSelector((status) => status.player);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadPlayersThunk());
+  });
 
   return (
     <ListPlayersStyles>
